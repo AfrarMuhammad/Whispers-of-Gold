605fe0389f5c8070ae411e8302106fc25c3b3952
AfrarMuhammad <156886332+AfrarMuhammad@users.noreply.github.com>
2025-04-21 15:47:53 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-$servername = "localhost";
+$servername = "my-mysql";
 $username = "root";
 $password = "root";
 $dbname = "Jewllery";
-$port = 3307;
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
